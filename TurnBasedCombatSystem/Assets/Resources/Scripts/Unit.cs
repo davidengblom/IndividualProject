63b458a4b69d5e5ca9647206a1c51473e9e12764
David Engblom <david.engblom@student.forsbergsskola.se>
2020-07-08 16:27:27 +0200

    Version 0.2
    Added UI, a few test sprites. The ability to attack, miss and critical strike. Added shake on the player sprites when they get hit, the shake more if the hit was critical.

---
+++ TurnBasedCombatSystem/Assets/Resources/Scripts/Unit.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string unitName;
+    public int unitLevel;
+
+    public int damage;
+    public int maxHP;
+    public int currentHP;
+    public float critChance;
+    public float hitChance;
+
+    private Vector3 originPosition;
+    private Quaternion originRotation;
+    public float shakeDecay = 0.002f;
+    public float shakeIntensity = 0.3f;
+
+    private float tempShakeIntensity = 0;
+
+    void Update()
+    { 
+        if (tempShakeIntensity > 0)
+        {
+            transform.position = originPosition + Random.insideUnitSphere * tempShakeIntensity;
+            transform.rotation = new Quaternion(
+                originRotation.x + Random.Range(-tempShakeIntensity, tempShakeIntensity) * 0.2f,
+                originRotation.y + Random.Range(-tempShakeIntensity, tempShakeIntensity) * 0.2f,
+                originRotation.z + Random.Range(-tempShakeIntensity, tempShakeIntensity) * 0.2f,
+                originRotation.w + Random.Range(-tempShakeIntensity, tempShakeIntensity) * 0.2f);
+            tempShakeIntensity -= shakeDecay;
+        }
+    }
+
+    public bool TakeDamage(int damage)
+    {
+        currentHP -= damage;
+        
+        if(currentHP <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Shake()
+    {
+        originPosition = transform.position;
+        originRotation = transform.rotation;
+        tempShakeIntensity = shakeIntensity;
+    }
+}
